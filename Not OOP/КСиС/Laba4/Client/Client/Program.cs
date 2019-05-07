740c2b192286409b3e22be7a99c6f7bc99d991b4
Cothn <Sevkaa-0013@mail.ru>
2019-05-07 23:31:48 +0400

    ssl version
    0.0.1

--- Not OOP/КСиС/Laba4/Client/Client/Program.cs
+++ Not OOP/КСиС/Laba4/Client/Client/Program.cs
@@ -7,6 +7,16 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
 namespace Client
 {
 
@@ -17,8 +27,7 @@ namespace Client
 
             try
             {
-
-                while (SendMessageFromSocket("smtp.mail.ru", 25)) ;
+                while (SendMessageFromSocket("smtp.yandex.ru", 25)) ;
 
             }
             catch (Exception ex)
@@ -30,40 +39,88 @@ namespace Client
                Console.ReadLine();
             }
         }
+        void SendCommand(string command, SslStream Stream)
+        {
 
+            byte[] commandBytes = System.Text.Encoding.ASCII.GetBytes(String.Format("{0}\r\n", command));
+
+            // Пишем команду для сервера
+            Stream.Write(commandBytes, 0, commandBytes.Length);
+            Stream.Flush(); // Записывает содержимое потока
+        }
         static bool SendMessageFromSocket(string Host, int port){
             
             bool otv = false;
-            byte[] BSendMess;
-            int bytesSent;
-            byte[] BRecMess;
-            int butesRec;
-            String SRecMess;
+            //byte[] BSendMess;
+            //int bytesSent;
+            //byte[] BRecMess;
+            //int butesRec;
+            //String SRecMess;
             IPHostEntry ipHost = null;
 
             //установка удаленной точки для сокета 217.69.139.160 //94.100.180.160
             ipHost = Dns.GetHostEntry(Host);
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+            TcpClient client = new TcpClient();
+            client.Connect(ipEndPoint);
+            //TcpClient client = new TcpClient(Host, port);
+            //SslStream sslStream = new SslStream(client.GetStream(), false);
+            //sslStream.ReadTimeout = 5000;
+            //sslStream.WriteTimeout = 5000;
+            SslStream sslStream;
+            sslStream = new SslStream(client.GetStream(), false);
+            // Аутентификация
+            try
+            {
+                sslStream.AuthenticateAsClient(Host);
+            }
+            catch (Exception ex)
+            {
+                client.Close();
+                throw ex;
+            }
+
+            if (sslStream.IsAuthenticated == false)
+            {
+                client.Close();
+                throw new Exception("Аутентификация не была пройдена.");
+            }
+            sslStream.ReadTimeout = 500;  // Тут еще не определился, какой таймаут выставлять
+            sslStream.WriteTimeout = 500;
 
             //Сoздаем сокет Tcp/Ip
-            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
+            //Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
             //Соеденяемся с удаленной точкой
-            sender.Connect(ipEndPoint);
+            //sender.Connect(ipEndPoint);
             Console.WriteLine("Сокет соединяется с {0}", ipEndPoint);
-            BRecMess = new byte[1024];
-            butesRec = sender.Receive(BRecMess);
-            SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
-            Console.WriteLine(SRecMess);
+            //BRecMess = new byte[1024];
 
-            //получаем свой ip
+            ////string messageData = ReadMessage(sslStream);
+            ////Console.WriteLine("Received: {0}", messageData);
+            //butesRec = sender.Receive(BRecMess);
+            //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
+            //Console.WriteLine(SRecMess);
+
+            ////получаем свой ip
             string strHostName = Dns.GetHostName();
             IPHostEntry ipHostC = Dns.GetHostEntry(strHostName);
             IPAddress ipAddrC = ipHost.AddressList[0];
 
-            // запуск TSL
+
+
+            //// начало общения
+            //BSendMess = Encoding.UTF8.GetBytes("EHLO " + ipAddrC.ToString() + "\r\n");
+            ////BSendMess = Encoding.UTF8.GetBytes("HELO "+"<cothnpir@mail.ru>"+"\r\n");
+            ////BSendMess = Encoding.UTF8.GetBytes("HELO " + "smtp.testbox" + "\r\n");
+            //bytesSent = sender.Send(BSendMess);
+            //BRecMess = new byte[4096];
+            //butesRec = sender.Receive(BRecMess);
+            //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
+            //Console.WriteLine(SRecMess);
+
+            //// запуск TSL
             //BSendMess = Encoding.UTF8.GetBytes("STARTTLS" + "\r\n");
             //bytesSent = sender.Send(BSendMess);
             //BRecMess = new byte[1024];
@@ -71,68 +128,58 @@ namespace Client
             //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
             //Console.WriteLine(SRecMess);
 
-            // начало общения
-            BSendMess = Encoding.UTF8.GetBytes("EHLO " + ipAddrC.ToString() + "\r\n");
-            //BSendMess = Encoding.UTF8.GetBytes("HELO "+"<cothnpir@mail.ru>"+"\r\n");
-            //BSendMess = Encoding.UTF8.GetBytes("HELO " + "smtp.testbox" + "\r\n");
-            bytesSent = sender.Send(BSendMess);
-            BRecMess = new byte[4096];
-            butesRec = sender.Receive(BRecMess);
-            SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
-            Console.WriteLine(SRecMess);
-
-            // Аутентификация
-            //BSendMess = Encoding.UTF8.GetBytes("AUTH LOGIN <sevkaa>" + "\r\n");
+            //// Аутентификация
+            //BSendMess = Encoding.UTF8.GetBytes("AUTH" + "\r\n");
             //bytesSent = sender.Send(BSendMess);
             //BRecMess = new byte[1024];
             //butesRec = sender.Receive(BRecMess);
             //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
             //Console.WriteLine(SRecMess);
 
-            // Отправитель
-            BSendMess = Encoding.UTF8.GetBytes("MAIL FROM:<" + "mymail@list.ru" + ">\r\n");
-            bytesSent = sender.Send(BSendMess);
-            BRecMess = new byte[1024];
-            butesRec = sender.Receive(BRecMess);
-            SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
-            Console.WriteLine(SRecMess);
+            //// Отправитель
+            //BSendMess = Encoding.UTF8.GetBytes("MAIL FROM:<" + "sevkaa-0013@mail.ru" + ">\r\n");
+            //bytesSent = sender.Send(BSendMess);
+            //BRecMess = new byte[1024];
+            //butesRec = sender.Receive(BRecMess);
+            //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
+            //Console.WriteLine(SRecMess);
 
-            //Получатель
-            BSendMess = Encoding.UTF8.GetBytes("RCPT TO:" + "<sevkaa0013@gmail.com>" + "\r\n");
-            bytesSent = sender.Send(BSendMess);
-            BRecMess = new byte[1024];
-            butesRec = sender.Receive(BRecMess);
-            SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
-            Console.WriteLine(SRecMess);
+            ////Получатель
+            //BSendMess = Encoding.UTF8.GetBytes("RCPT TO:" + "<sevkaa-0013@mail.ru>" + "\r\n");
+            //bytesSent = sender.Send(BSendMess);
+            //BRecMess = new byte[1024];
+            //butesRec = sender.Receive(BRecMess);
+            //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
+            //Console.WriteLine(SRecMess);
 
-            //Сообщаем о начале письма
-            BSendMess = Encoding.UTF8.GetBytes("DATA"+"\r\n");
-            bytesSent = sender.Send(BSendMess);
-            BRecMess = new byte[1024];
-            butesRec = sender.Receive(BRecMess);
-            SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
-            Console.WriteLine(SRecMess);
+            ////Сообщаем о начале письма
+            //BSendMess = Encoding.UTF8.GetBytes("DATA"+"\r\n");
+            //bytesSent = sender.Send(BSendMess);
+            //BRecMess = new byte[1024];
+            //butesRec = sender.Receive(BRecMess);
+            //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
+            //Console.WriteLine(SRecMess);
 
-            //Письмо
-            BSendMess = Encoding.UTF8.GetBytes("Test Cotn" + "\r\n." + "\r\n");
-            bytesSent = sender.Send(BSendMess);
-            BRecMess = new byte[1024];
-            butesRec = sender.Receive(BRecMess);
-            SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
-            Console.WriteLine(SRecMess);
+            ////Письмо
+            //BSendMess = Encoding.UTF8.GetBytes("Test Cotn" + "\r\n." + "\r\n");
+            //bytesSent = sender.Send(BSendMess);
+            //BRecMess = new byte[1024];
+            //butesRec = sender.Receive(BRecMess);
+            //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
+            //Console.WriteLine(SRecMess);
 
-            //Конец
-            BSendMess = Encoding.UTF8.GetBytes("QUIT"+"\r\n");
-            bytesSent = sender.Send(BSendMess);
-            BRecMess = new byte[1024];
-            butesRec = sender.Receive(BRecMess);
-            SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
-            Console.WriteLine(SRecMess);
+            ////Конец
+            //BSendMess = Encoding.UTF8.GetBytes("QUIT"+"\r\n");
+            //bytesSent = sender.Send(BSendMess);
+            //BRecMess = new byte[1024];
+            //butesRec = sender.Receive(BRecMess);
+            //SRecMess = Encoding.UTF8.GetString(BRecMess, 0, butesRec - 1);
+            //Console.WriteLine(SRecMess);
 
 
-            //Освобождаем сокет
-            sender.Shutdown(SocketShutdown.Both);
-            sender.Close();
+            ////Освобождаем сокет
+            //sender.Shutdown(SocketShutdown.Both);
+            //sender.Close();
 
 
             return otv;
--- Not OOP/КСиС/Laba4/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Not OOP/КСиС/Laba4/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,35 +1,152 @@
 ﻿using System;
-using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.IO;
-using System.Threading.Tasks;
-using System.Net.Mail;
 
+using System.Net;
+using System.Net.Sockets;
 namespace NetConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string[] ListOfFile = new string[100];
+            int KolFile = ReadListFile(ListOfFile);
 
-            // отправитель - устанавливаем адрес и отображаемое в письме имя
-            MailAddress from = new MailAddress("somemail@gmail.com", "Tom");
-            // кому отправляем
-            MailAddress to = new MailAddress("somemail@yandex.ru");
-            // создаем объект сообщения
-            MailMessage m = new MailMessage(from, to);
-            // тема письма
-            m.Subject = "Тест";
-            // текст письма
-            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
-            // письмо представляет код html
-            m.IsBodyHtml = true;
-            // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            // логин и пароль
-            smtp.Credentials = new NetworkCredential("somemail@gmail.com", "mypassword");
-            smtp.EnableSsl = true;
-            smtp.Send(m);
-            Console.Read();
+            //конечная локальная точка
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[1];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 25);
+
+            //Сoздаем сокет Ncp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //Сокет для локальной точки и прослушивание входящих сокетов
+            try
+            {
+
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(7);
+
+                //начинаем слушать
+                while (true)
+                {
+
+                    //!!!
+                    Console.WriteLine("Ожидаем соединения через {0}", ipEndPoint);
+
+                    //Ожидаем соединения
+                    Socket handler = sListener.Accept();
+                    string data = null;
+
+                    //общаемся с клиентом
+                    //Отвечаем
+                    string reply = "220 LocalMail SMTP is ready \r\n";
+                    byte[] replyBuf = Encoding.UTF8.GetBytes(reply);
+                    int bytesSent = handler.Send(replyBuf);
+
+                    //получаем
+                    byte[] bytes = new byte[1024];
+                    int butesRec = handler.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, butesRec);
+                    //Отвечаем
+                    reply = "250 LocalMail\r\n";
+                    replyBuf = Encoding.UTF8.GetBytes(reply);
+                    bytesSent = handler.Send(replyBuf);
+
+                    while (data != "QUIT\r\n")
+                    {
+                        bytesSent = handler.Send(replyBuf);
+                        //получаем
+                        bytes = new byte[1024];
+                        butesRec = handler.Receive(bytes);
+                        data += Encoding.UTF8.GetString(bytes, 0, butesRec);
+                        //switch (data) { 
+                         //   case "":
+                        
+                        
+                        
+                        
+                        //}
+
+                        //Отвечаем
+                        reply = "250 OK\r\n";
+                        replyBuf = Encoding.UTF8.GetBytes(reply);
+                    }
+
+                        reply = "221 LocalMail is closing chanel\r\n";
+                        replyBuf = Encoding.UTF8.GetBytes(reply);
+        
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+            //CreateListFile(ListOfFile, kolStr);
+        }
+
+
+        //static public void CreateListFile(string[] FileList, int kolStr)
+        //{
+
+        //    FileStream file1 = new FileStream("ServFile\\test.txt", FileMode.Create); //создаем файловый поток
+        //    StreamWriter writer = new StreamWriter(file1); //создаем «поток записиь» и связываем его с файловым потоком
+
+        //    int i=0;
+        //    while (i < kolStr)
+        //    {
+        //        writer.Write(FileList[i]); //записываем в файл
+        //    }
+
+        //    writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется 
+
+        //}
+
+        static public int ReadListFile(string[] FileList)
+        {
+
+            FileStream file1 = new FileStream("ServFile\\FileList.txt", FileMode.Open); //создаем файловый поток
+            StreamReader reader = new StreamReader(file1); //создаем «экземпляр чтения» и связываем его с файловым потоком
+
+            int i = 0;
+            while (!reader.EndOfStream && i < 100)
+            {
+                FileList[i] = reader.ReadLine(); //read
+                i++;
+            }
+
+            reader.Close(); //закрываем поток.
+            return i;
+
+        }
+
+        static public bool CheckListFile(string[] FileList, int kolFile, string sign)
+        {
+
+            int i = 0;
+            while (i < kolFile)
+            {
+                if (FileList[i] == sign)
+                {
+                    return true;
+                }
+                i++;
+            }
+
+            return false;
+
         }
     }
 }
